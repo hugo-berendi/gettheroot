14acbe00c6b8a0654c8ce4a92cf42e6689db98c9
Kamachi <68025706+hugo-berendi@users.noreply.github.com>
2023-12-01 12:05:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -89,3 +89,35 @@ function doForm() {
 
 	return false;
 }
+
+function test() {
+	let l = 10;
+	let t = 0;
+	let i = 2;
+	let r = (i + i / i) / 2;
+
+	while (l > t) {
+		r = (r + i / r) / 2;
+		if (r * r == i) {
+			console.log(r);
+		} else {
+			if (t == l) {
+				console.log(r);
+			} else {
+				t++;
+			}
+		}
+	}
+}
+
+function log(text, type) {
+	if (type == 'error') {
+		console.error(text);
+	} else if (type == 'warn') {
+		console.warn(text);
+	} else if (type == 'info') {
+		console.info(text);
+	} else if (type == 'log') {
+		console.log(text);
+	}
+}
